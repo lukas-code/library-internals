e0b03a7b02a24a8647deede76e29e0bbbc6eda82
Lukas Markeffsky <@>
2024-09-03 02:14:27 +0200

    add version number

--- doc.sh
+++ doc.sh
@@ -2,10 +2,12 @@
 set -ex
 rustup toolchain install nightly --component rust-src
 sysroot=$(rustc +nightly --print sysroot)
-RUSTDOCFLAGS='-Z unstable-options
+version=$(rustc +nightly --version | cut -d ' ' -f 2- | tr ' ' '\t')
+RUSTDOCFLAGS="-Z unstable-options
     --document-private-items
     --document-hidden-items
     --show-type-layout
     --enable-index-page
-    ' \
+    --crate-version ${version}
+    " \
     cargo +nightly doc --manifest-path "$sysroot/lib/rustlib/src/rust/library/Cargo.toml" --target-dir target
